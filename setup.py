d5d719df73fe8346c3aa8ed9c9cb57dd3a9894b0
exesse <kulbatski@gmail.com>
2021-01-05 16:38:19 +0100

    release 1.0.5

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="mongodump-s3",
-    version="1.0.4",
+    version="1.0.5",
     description="Read the latest Real Python tutorials",
     long_description=README,
     long_description_content_type="text/markdown",
