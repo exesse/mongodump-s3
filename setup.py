3bfb68d80b4dd9f73902d79a6929552fafd45030
exesse <kulbatski@gmail.com>
2021-01-05 15:44:39 +0100

    recover

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="mongodump-s3",
-    version="1.0.1",
+    version="1.0.0",
     description="Read the latest Real Python tutorials",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -23,11 +23,11 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
     ],
-    packages=["mongodump_s3"],
+    packages=["mongodump-s3"],
     include_package_data=False,
     install_requires=[
         "requests==2.25.1",
-        "hurry.filesize",
+        "hurry.filesize==0.9",
         "python-dotenv==0.15.0",
         "azure-storage-blob==12.6.0",
         "boto3==1.16.48",
